9e484a7ac16e6873a83a0ed13fc3fc3381097ba7
Luke Lefebure <luke.lefebure@gmail.com>
2018-09-23 00:22:35 +0000

    update tree model

--- models/rf/train_rf_model.py
+++ models/rf/train_rf_model.py
@@ -3,6 +3,7 @@ import pandas as pd
 import dask.dataframe as dd
 import numpy as np
 import gcsfs
+import re
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.metrics import mean_squared_error
 
@@ -34,19 +35,29 @@ print('Number of estimators per batch: %d' % n_estimators_per_batch, flush=True)
 # Define features to include in model
 features = ['distance', 'duration', 'year', 'month', 'dow', 'hour', 'bearing_bucket',
             'pickup_latitude', 'pickup_longitude', 'dropoff_latitude',
-            'dropoff_longitude', 'pickup_jfk', 'dropoff_jfk', 'summary1', 'summary2']
+            'dropoff_longitude', 'pickup_jfk', 'dropoff_jfk', 'summary1', 'summary2',
+            'same_pickup_dropoff']
 
 # Set levels of categorical variables (infer from full dev set)
 dev = (dd.read_csv('gs://nyc-taxi-fare-prediction-data/dev.csv', dtype=dtypes)
        .compute())
 def convert_to_categoricals(df):
-    toll_roads = ['Queens-Midtown Tunnel', 'Robert F. Kennedy Bridge',
-                  'Henry Hudson Parkway']
+    top_roads = ['Queens-Midtown Tunnel', 'FDR Drive', 'Park Avenue',
+                 'Madison Avenue', 'ith Avenue', 'Lexington Avenue',
+                 'Central Park West']
     for col in dtypes:
         if dtypes[col] == 'str':
             df[col] = pd.Categorical(df[col], dev[col].unique())
-    df['summary1'] = pd.Categorical(df.summary1, toll_roads)
-    df['summary2'] = pd.Categorical(df.summary2, toll_roads)
+    ith_avenue_pattern = r'[0-9]{1,2}(th|nd|rd|st) Avenue'
+    def _encode_summary(x):
+        if type(x) == str and re.match(ith_avenue_pattern, x):
+            return 'ith Avenue'
+        else:
+            return x
+    df['summary1'] = pd.Categorical(
+        df.summary1.apply(_encode_summary), top_roads)
+    df['summary2'] = pd.Categorical(
+        df.summary2.apply(_encode_summary), top_roads)
 
 convert_to_categoricals(dev)
 dev_transformed = pd.get_dummies(dev[features])
@@ -72,7 +83,7 @@ with fs.open('nyc-taxi-fare-prediction-data/train.csv') as f:
 print('\nFeature Importance\n----------------')
 for f, imp in sorted(zip(dev_transformed.columns, model.feature_importances_), 
                      key=lambda x: x[1], reverse=True):
-    print('%s: %d' % (f, imp), flush=True)
+    print('%s: %.4f' % (f, imp), flush=True)
 
 # Test predictions
 test = (dd.read_csv('gs://nyc-taxi-fare-prediction-data/test.csv', dtype=dtypes)
@@ -83,4 +94,4 @@ test_preds = pd.DataFrame({
     'key': test.key,
     'fare_amount': model.predict(pd.get_dummies(test_transformed))
 })
-pd.to_csv('prediction.csv', index=False)
+test_preds.to_csv('prediction.csv', index=False)
